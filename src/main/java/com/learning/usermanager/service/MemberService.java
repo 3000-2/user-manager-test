38c29b65be7083f985981bd53057a1d49dadb89f
3000-2 <psch0493@gmail.com>
2022-08-16 17:40:52 +0900

    feature: add member service

---
+++ src/main/java/com/learning/usermanager/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.learning.usermanager.service;
+
+import com.learning.usermanager.domain.Member;
+import com.learning.usermanager.repository.MemberRepository;
+import com.learning.usermanager.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+
+        // 같은 이름을 가진 회원이 존재할 경우
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
