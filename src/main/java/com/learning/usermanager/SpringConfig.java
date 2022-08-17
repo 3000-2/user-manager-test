bc0a8c0c1d12411876bb543dc30524dc83d7c148
3000-2 <psch0493@gmail.com>
2022-08-17 20:24:59 +0900

    fix: directly inject beans

---
+++ src/main/java/com/learning/usermanager/SpringConfig.java
@@ -0,0 +1,25 @@
+package com.learning.usermanager;
+
+import com.learning.usermanager.repository.MemberRepository;
+import com.learning.usermanager.repository.MemoryMemberRepository;
+import com.learning.usermanager.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * component scan 방식이 아닌 직접 bean 등록하기
+ * */
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- src/main/java/com/learning/usermanager/service/MemberService.java
+++ src/main/java/com/learning/usermanager/service/MemberService.java
@@ -2,18 +2,15 @@ package com.learning.usermanager.service;
 
 import com.learning.usermanager.domain.Member;
 import com.learning.usermanager.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
